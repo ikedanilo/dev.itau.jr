1eb4ab338a48263405a38eb0da68ab4a2ad0d3bd
ikedanilo <daniloikeda@hotmail.com>
2025-06-29 09:40:08 -0300

    Update Transacao.java

--- src/main/java/com/desafio/dev/itau/jr/model/Transacao.java
+++ src/main/java/com/desafio/dev/itau/jr/model/Transacao.java
@@ -1,6 +1,8 @@
 package com.desafio.dev.itau.jr.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +21,10 @@ public class Transacao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @NotNull
+    @Min(0)
     private Double valor;
 
-    @Column(nullable = false)
+    @NotNull
     private OffsetDateTime dataHora;
 }
